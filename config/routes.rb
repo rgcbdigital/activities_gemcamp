0c3fd03d93354d5c35b4d0c1f448f9161f57ebc9
Cheysser <cheysser@gmail.com>
2024-10-05 10:15:20 -0700

    Added Nav bar and other pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root 'welcome#index'
+  # root "articles#index"
+  root 'welcome#index'
+  get 'about_us', to: 'welcome#about_us'
+  get 'contact', to: 'welcome#contact'
 end
