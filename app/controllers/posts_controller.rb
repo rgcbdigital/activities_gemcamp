ee698ae0505853486000d0369900df5ab3549952
Cheysser <cheysser@gmail.com>
2024-10-05 11:11:41 -0700

    Added post show, edit and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,40 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # render json: params[:post]
+    # @post = Post.new
+    # @post.title = params[:post][:title]
+    # @post.content = params[:post][:content]
+    # @post = Post.new(title: params[:post][:title],
+    #                  content: params[:post][:content])
+    @post = Post.new(params[:post].permit(:title, :content))
+    if @post.save
+      redirect_to posts_path
+    end
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :content))
+      redirect_to posts_path
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
   get 'about_us', to: 'welcome#about_us'
   get 'contact', to: 'welcome#contact'
   get 'posts', to: 'posts#index'
+  post 'posts', to: 'posts#create'
+  get 'posts/new', to: 'posts#new'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  patch 'posts/:id', to: 'posts#update'
+  put 'posts/:id', to: 'posts#update'
+  delete 'posts/:id', to: 'posts#destroy'
 end
